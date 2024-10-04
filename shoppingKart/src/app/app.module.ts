038399b5518e258ed745ca8ffea5bb89300903ce
ankkit24 <ankit.n24@gmail.com>
2024-10-03 20:54:48 -0400

    Added topheader component

--- shoppingKart/src/app/app.module.ts
+++ shoppingKart/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopheaderComponent } from './topheader/topheader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopheaderComponent
   ],
   imports: [
     BrowserModule,
